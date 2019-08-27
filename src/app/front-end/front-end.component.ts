a1da1f28b6b6f8bd3c087afb868ecb7f324f31f3
GLENNSURFACE02\glenn <glenn@glennpacker.net>
2019-08-27 20:04:26 +0100

    front end animation error fixed

--- src/app/front-end/front-end.component.ts
+++ src/app/front-end/front-end.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { routerTransition } from '../router.animations';
 
 @Component({
   selector: 'app-front-end',
   templateUrl: './front-end.component.html',
-  styleUrls: ['./front-end.component.scss']
+  styleUrls: ['./front-end.component.scss'],
+  animations: [routerTransition()]
 })
 export class FrontEndComponent implements OnInit {
 
